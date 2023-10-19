bcfdf05f8f33123c77e00fcef8be117410cbb272
Cedric Ziel <cedric@cedric-ziel.com>
2023-10-19 18:19:26 +0200

    Add k6

---
+++ docker/k6/script.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export const options = {
+    vus: 10,
+    duration: '30s',
+};
+
+export default function () {
+    http.get(`http://${__ENV.WEB_HOST}`);
+    sleep(1);
+}
