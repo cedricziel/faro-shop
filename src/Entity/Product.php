6ea005d064fd064751e2e338f5327f43170d19b3
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-12 15:26:26 +0100

    chore: add faros fixtures (#168)

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -23,6 +23,9 @@ class Product
     #[ORM\Column]
     private ?float $price = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +63,18 @@ class Product
     public function setPrice(float $price): static
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
