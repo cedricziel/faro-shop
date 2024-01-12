9bef726e11cb9963511fb8a80686ef6bc63e8237
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-12 16:28:17 +0100

    fix: Allow fixtures bundle in production (#175)

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
