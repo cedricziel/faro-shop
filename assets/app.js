a30500d14fb1433e9cd34039071a604cfc2b8bb8
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-18 14:50:33 +0100

    fix: initialize faro directly (#192)
    Signed-off-by: Cedric Ziel <cedric@cedric-ziel.com>

--- assets/app.js
+++ assets/app.js
@@ -2,8 +2,4 @@ import './styles/app.css';
 
 import {initializeFaro} from './scripts/faro';
 
-document.onreadystatechange = () => {
-    if (document.readyState === "complete") {
-        global.faro = initializeFaro();
-    }
-};
+initializeFaro();
