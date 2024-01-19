1f8c38d4e30795b39f7c335a520cafe0ca302170
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-19 13:50:47 +0100

    chore: add default quantity to cart

--- src/Form/AddToCartType.php
+++ src/Form/AddToCartType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\OrderItem;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,7 +13,15 @@ class AddToCartType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('quantity');
+        $builder->add('quantity', NumberType::class, [
+            'label' => 'Quantity',
+            'attr' => [
+                'min' => 1,
+                'max' => 10,
+                'step' => 1,
+            ],
+            'data' => 1,
+        ]);
         $builder->add('add', SubmitType::class, [
             'label' => 'Add to cart'
         ]);
