6d2e61e6afdeab555d1256b8fcdbd1037af37bd5
Cedric Ziel <cedric@cedric-ziel.com>
2024-06-23 09:35:51 +0200

    feat: fail server transactions

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\StatusCode;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,9 +22,21 @@ class AdController extends AbstractController
     {
         $products = $this->productRepository->findAdvertised(3);
 
+        $response = new Response();
+        try {
+            if (rand(1, 10) === 1) {
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+                throw new BadRequestException('Unable to fetch ads for homepage');
+            }
+        } catch (\Exception $e) {
+            Span::getCurrent()
+                ->recordException($e)
+                ->setStatus(StatusCode::STATUS_ERROR, 'Unable to fetch ads for homepage');
+        }
+
         return $this->render('ad/ads_for_home.html.twig', [
             'products' => $products,
-        ]);
+        ], $response);
     }
 
     #[Route('/ads/{id}', name: 'ads_for_product')]
@@ -30,9 +44,21 @@ class AdController extends AbstractController
     {
         $products = $this->productRepository->findAdvertised(3);
 
+        $response = new Response();
+        try {
+            if ($product->getId() % 2 === 1) {
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+                throw new BadRequestException('Product not allowed to show ads');
+            }
+        } catch (\Exception $e) {
+            Span::getCurrent()
+                ->recordException($e)
+                ->setStatus(StatusCode::STATUS_ERROR, 'Product not allowed to show ads');
+        }
+
         return $this->render('ad/ads_for_product.html.twig', [
             'product' => $product,
             'products' => $products,
-        ]);
+        ], $response);
     }
 }
