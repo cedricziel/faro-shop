b9ec21738a1e6da1547da8435f85c2cb6cc33547
Cedric Ziel <cedric@cedric-ziel.com>
2024-06-27 16:59:47 +0200

    fix: generate ids in the database (#455)

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Product
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
     #[ORM\Column]
     private ?int $id = null;
 
