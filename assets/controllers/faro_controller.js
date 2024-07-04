89a7c834bfbf8927601f8e9639df1828c28ca688
Cedric Ziel <cedric@cedric-ziel.com>
2024-07-04 12:16:46 +0200

    feat: move to Faro controller (#479)

--- assets/controllers/faro_controller.js
+++ assets/controllers/faro_controller.js
@@ -1,12 +1,8 @@
-import {Controller} from "@hotwired/stimulus";
+import { Controller } from '@hotwired/stimulus';
 import {faro} from "@grafana/faro-web-sdk";
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-    static values = {
-        message: String
-    };
-
     onError(event) {
         faro.api.pushError(event.detail.error);
     }
