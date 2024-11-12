70392a070c28bfad94789e93865d241b711b3bfb
Cedric Ziel <cedric@cedric-ziel.com>
2024-11-12 11:22:04 +0100

    feat: introduce redis cache

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,35 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
-use OpenTelemetry\API\Trace\TracerInterface;
+use App\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    public function __construct(private TracerInterface $tracer)
-    {
+    public function __construct(
+        private ProductService $productService,
+    ){
     }
 
     #[Route('/', name: 'home')]
     public function index(ProductRepository $productRepository): Response
     {
-        // 5 minutes after the full hour, and 5 minutes after the half hour, slow down requests for demo purposes
-        $currentMinute = (int)date('i');
-        if (($currentMinute >= 0 && $currentMinute <= 5) || ($currentMinute >= 30 && $currentMinute <= 35)) {
-            $span = $this->tracer->spanBuilder('cache.refresh')
-                ->startSpan();
-
-            $span->addEvent('cache.refresh.start');
-            sleep(1);
-            $span->addEvent('cache.refresh.end');
-
-            $span->end();
-        }
+        $products = $this->productService->findAll();
 
         return $this->render('home/index.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $products,
         ]);
     }
 }
