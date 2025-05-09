70de17a8dd2cce5a0a6ca5075c2acede6e350d90
Cedric Ziel <cedric@cedric-ziel.com>
2025-05-09 16:06:31 +0200

    chore: enable geolocation
    Signed-off-by: Cedric Ziel <cedric@cedric-ziel.com>

--- assets/scripts/faro.js
+++ assets/scripts/faro.js
@@ -48,6 +48,7 @@ export function initializeFaro() {
         },
         trackWebVitalsAttribution: true,
         trackUserActionsPreview: true,
-        trackResources: true
+        trackResources: true,
+        trackGeolocation: true
     });
 }
