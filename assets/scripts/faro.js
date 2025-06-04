0675d65da007f9060d33bf8ad37131bd79a4eefb
marco.schaefer <marco.schaefer@grafana.com>
2025-06-04 13:46:56 +0200

    chore(faro script): clean up

--- assets/scripts/faro.js
+++ assets/scripts/faro.js
@@ -1,54 +1,56 @@
-import { getWebInstrumentations, initializeFaro as initializeFaroReal } from '@grafana/faro-web-sdk';
-import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+import {
+  getWebInstrumentations,
+  initializeFaro as initializeFaroReal,
+} from "@grafana/faro-web-sdk";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 
-const appVersion = window.version || 'unknown';
+const appVersion = window.version || "unknown";
 const faroPage = window.faroPageMeta || {};
 const faroUrl = window.faroUrl || null;
 const faroNamespace = window.faroNamespace || undefined;
 const faroCountry = window.faroCountry || undefined;
 
 export function initializeFaro() {
-    return initializeFaroReal({
-        beforeSend: (item) => {
-            delete item.meta.k6;
+  return initializeFaroReal({
+    beforeSend: (item) => {
+      delete item.meta.k6;
 
-            return item;
-        },
-        url: faroUrl,
-        app: {
-            name: 'faro-shop-frontend',
-            namespace: faroNamespace,
-            version: appVersion,
-            environment: 'production'
-        },
-        instrumentations: [
-            // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
-            ...getWebInstrumentations(),
+      return item;
+    },
+    url: faroUrl,
+    app: {
+      name: "faro-shop-frontend",
+      namespace: faroNamespace,
+      version: appVersion,
+      environment: "production",
+    },
+    instrumentations: [
+      // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
+      ...getWebInstrumentations(),
 
-            // Initialization of the tracing package.
-            // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
-            new TracingInstrumentation({
-                resourceAttributes: {
-                    ['geo.country']: faroCountry,
-                }
-            }),
-        ],
-        metas: [
-            () => ({
-                page: faroPage,
-                session: {
-                    attributes: {
-                        country: faroCountry,
-                    }
-                },
-            }),
-        ],
-        sessionTracking: {
-            persistent: true,
+      // Initialization of the tracing package.
+      // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
+      new TracingInstrumentation({
+        resourceAttributes: {
+          ["geo.country"]: faroCountry,
         },
-        trackWebVitalsAttribution: true,
-        trackUserActionsPreview: true,
-        trackResources: true,
-        trackGeolocation: true
-    });
+      }),
+    ],
+    sessionTracking: {
+      persistent: true,
+      // setting the initial session attributes
+      session: {
+        attributes: {
+          country: faroCountry,
+        },
+      },
+    },
+    pageTracking: {
+      generatePageId() {
+        return faroPage.id;
+      },
+    },
+    trackUserActionsPreview: true,
+    trackResources: true,
+  });
 }
